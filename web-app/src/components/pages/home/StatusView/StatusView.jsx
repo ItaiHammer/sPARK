ac29fe51708138a43df03d71ce31ac02b9430d8a
Itai Hammer <itaihammer1@gmail.com>
2025-12-15 15:15:27 -0800

    feat(StatusView and GarageCard): Made them wrap and scale to look better on pc

--- web-app/src/components/pages/home/StatusView/StatusView.jsx
+++ web-app/src/components/pages/home/StatusView/StatusView.jsx
@@ -25,73 +25,79 @@ import FilterButtons from "./FilterButtons";
 import carAnimation from "@/animations/car_loading_animation.json";
 
 export default function StatusViewPage({ locationId }) {
-  const {
-    timeFilterMenu: { date, type },
-  } = useUI();
+    const {
+        timeFilterMenu: { date, type },
+    } = useUI();
 
-  // Get Forecast Points
-  const { getLatestOccupancy } = useLocationAPI();
-  const { getForecastPoints } = useForecastAPI();
-  const {
-    data: rawData,
-    error,
-    isLoading,
-  } = useSWR(
-    [
-      type === FILTER_TYPES.LIVE.value
-        ? "live-occupancy"
-        : "forecasted-occupancy",
-      locationId,
-      date,
-      type,
-    ],
-    ([key, id, date, type]) => {
-      if (type === FILTER_TYPES.CUSTOM.value)
-        return getForecastPoints(id, date);
+    // Get Forecast Points
+    const { getLatestOccupancy } = useLocationAPI();
+    const { getForecastPoints } = useForecastAPI();
+    const {
+        data: rawData,
+        error,
+        isLoading,
+    } = useSWR(
+        [
+            type === FILTER_TYPES.LIVE.value
+                ? "live-occupancy"
+                : "forecasted-occupancy",
+            locationId,
+            date,
+            type,
+        ],
+        ([key, id, date, type]) => {
+            if (type === FILTER_TYPES.CUSTOM.value)
+                return getForecastPoints(id, date);
 
-      return getLatestOccupancy(id);
-    },
-    DEFAULT_SWR_OPTIONS
-  );
+            return getLatestOccupancy(id);
+        },
+        DEFAULT_SWR_OPTIONS
+    );
 
-  if (isLoading) {
-    return <Lottie animationData={carAnimation} style={{ height: 200 }} loop />;
-  }
+    if (isLoading) {
+        return (
+            <Lottie animationData={carAnimation} style={{ height: 200 }} loop />
+        );
+    }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
-  const data = rawData?.data || {};
-  const numOfLots = data?.lots?.length || 0;
+    const data = rawData?.data || {};
+    const numOfLots = data?.lots?.length || 0;
 
-  return (
-    <div className={styles.StatusViewPage}>
-      <div className={styles.GarageControls}>
-        <h2 className={styles.GaragesTitle}>
-          All Garages{" "}
-          {numOfLots > 0 && (
-            <motion.p
-              initial={{ opacity: 0, y: -10 }}
-              animate={{
-                opacity: 1,
-                y: 0,
-                transition: { duration: 0.3 },
-              }}
-            >
-              ({numOfLots})
-            </motion.p>
-          )}
-        </h2>
-        <FilterButtons />
-        <p className={styles.SortingIndicator}>Sorted by emptiest to fullest</p>
-      </div>
+    return (
+        <div className={styles.StatusViewPage}>
+            <div className={styles.GarageControls}>
+                <h2 className={styles.GaragesTitle}>
+                    All Garages{" "}
+                    {numOfLots > 0 && (
+                        <motion.p
+                            initial={{ opacity: 0, y: -10 }}
+                            animate={{
+                                opacity: 1,
+                                y: 0,
+                                transition: { duration: 0.3 },
+                            }}
+                        >
+                            ({numOfLots})
+                        </motion.p>
+                    )}
+                </h2>
+                <FilterButtons />
+                <p className={styles.SortingIndicator}>
+                    Sorted by emptiest to fullest
+                </p>
+            </div>
 
-      {numOfLots === 0
-        ? "There is no forecasted data for this time and date."
-        : data.lots.map((garage, i) => (
-            <GarageCard garage={garage} order={i} key={i} />
-          ))}
-    </div>
-  );
+            <div className={styles.GarageCardsContainer}>
+                {numOfLots === 0
+                    ? "There is no forecasted data for this time and date."
+                    : data.lots.map((garage, i) => (
+                          <GarageCard garage={garage} order={i} key={i} />
+                      ))}
+            </div>
+        </div>
+    );
 }
